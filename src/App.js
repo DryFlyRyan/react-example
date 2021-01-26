be3d9ba756b635020933820b45c64a04a7478bd9
Ryan Douglas <ryan.douglas@cakeinsure.com>
2021-01-25 17:24:16 -0700

    Initial Search Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import {
+  Search,
+} from './components';
 
 function App() {
+  // const [{ repositories }, dispatch] = useReducer(combinedReducer, updateStore())
+  const [previousSearchQuery, updatePreviousSearchQuery] = useState('')
+  const [searchQuery, updateSearchQuery] = useState('');
+  const [isSearching, updateIsSearching] = useState(false);
+  const [currentPage, updateCurrentPage] = useState(1);
+  const [searchSort, updateSearchSort] = useState({ value: '', label: 'Best Match'},);
+  const [searchOrder, updateSearchOrder] = useState({ value: 'desc', label: 'Descending'},) 
+
   return (
     <div className="App">
-      {/* <Search /> */}
+        <Search
+          isSearching={isSearching}
+          searchQuery={searchQuery}
+          onChange={e => updateSearchQuery(e.target.value)}
+          searchRepos={() => {}}
+          searchOrder={searchOrder}
+          searchSort={searchSort}
+          onOrderChange={() => {}}
+          onSortChange={() => {}}
+        />
       {/* <ContentWindow /> */}
     </div>
   );
---
+++ src/components/Search/Search.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Select from 'react-select';
+
+import {
+  Banner,
+  SearchInput,
+  HiddenLabel,
+  GrowContainer,
+  SearchForm,
+  LoadingIcon,
+  SelectContainer,
+} from './Search.styles';
+
+const searchSorts = [
+  { value: '', label: 'Best Match'},
+  { value: 'stars', label: 'Stars'},
+  { value: 'forks', label: 'Forks'},
+  { value: 'help-wanted-issues', label: 'Help Wanted Issues'},
+  { value: 'updated', label: 'Updated'},
+];
+
+const order = [
+  { value: 'desc', label: 'Descending'},
+  { value: 'asc', label: 'Ascending'},
+];
+
+const Search = ({
+  searchQuery,
+  onChange,
+  searchRepos,
+  isSearching,
+  searchOrder,
+  searchSort,
+  onOrderChange,
+  onSortChange,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return searchRepos();
+  }
+
+  const handleSearchSortChange = (selection) => onSortChange(selection);
+  const handleSearchOrderChange = (selection) => onOrderChange(selection);
+
+  return (
+    <Banner>
+      <GrowContainer>
+        <SearchForm
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <HiddenLabel>
+            <SearchInput
+              spellCheck="false"
+              autoComplete="off"
+              type="text"
+              name="search"
+              placeholder="Search ..."
+              value={searchQuery}
+              onChange={onChange}
+            />
+            <LoadingIcon
+              icon={faSpinner}
+              theme={{ isSearching }}
+            />
+          </HiddenLabel>
+        </SearchForm>
+      </GrowContainer>
+      <SelectContainer>
+        <Select
+          value={searchSort}
+          options={searchSorts}
+          onChange={handleSearchSortChange}
+        />
+        <Select
+          value={searchOrder}
+          options={order}
+          onChange={handleSearchOrderChange}
+          isDisabled={searchSort.value === ''}
+        />
+      </SelectContainer>
+    </Banner>
+  )
+};
+
+Search.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  searchRepos: PropTypes.func.isRequired,
+  isSearching: PropTypes.bool.isRequired,
+  searchOrder: PropTypes.shape().isRequired,
+  searchSort: PropTypes.shape().isRequired,
+  onOrderChange: PropTypes.func.isRequired,
+  onSortChange: PropTypes.func.isRequired,
+};
+
+export default Search;
